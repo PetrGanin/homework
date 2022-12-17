11c3cec3b03af3589e894e9fa7409105179a0e37
Petr_Ganin <stranik1985@gmail.com>
2022-12-17 16:49:35 +0300

    added cases

--- Program.cs
+++ Program.cs
@@ -4,18 +4,38 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер задачи от 1 до 4: ");
+int namber = Convert.ToInt32(Console.ReadLine());
+switch (namber)
+{
+    case 1:
+        Console.WriteLine("Ввод 2 числа и выдает max i min: ");
+        Console.WriteLine("Введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
 
-if (a > b) 
-{
-    Console.WriteLine("Это число большее: " + a );
-    Console.WriteLine("это число меньшее: " + b);
-}
-else
-{
-    Console.WriteLine("Это число большее: " + b );
-    Console.WriteLine("это число меньшее: " + a );
+        if (a > b)
+        {
+            Console.WriteLine("Это число большее: " + a);
+            Console.WriteLine("это число меньшее: " + b);
+        }
+        else
+        {
+            Console.WriteLine("Это число большее: " + b);
+            Console.WriteLine("это число меньшее: " + a);
+        }
+        break;
+
+    case 2:
+        break;
+
+    case 3:
+        break;
+
+    case 4:
+        break;
+
+    default:
+        break;
 }
